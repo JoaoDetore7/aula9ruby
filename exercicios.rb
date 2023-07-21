7b1633be755603aa8a29c451186a6d8170057e5d
JoaoDetore7 <joaodetore7@gmail.com>
2023-07-20 21:21:14 -0300

    exercicios: resolução

---
+++ exercicios.rb
@@ -0,0 +1,29 @@
+exercicio1
+numbers = [1,2,3,4,5,6,7,8,9,10]
+puts numbers
+
+exercicio2
+numbers.push(11,12)
+puts numbers
+
+exercicio3
+numbers.shift(1)
+puts numbers
+
+exercicio4
+puts "Contém" if numbers.include?(5)
+
+exercicio5
+aluno = {name: "João", age: 21, curso: "ADS"}
+puts aluno
+
+exercicio5
+aluno['semestre'] = 2
+puts aluno
+
+exercicio6
+aluno.delete(:age)
+puts aluno
+
+exercicio7
+puts "Contém" if aluno.has_key?(:age)
